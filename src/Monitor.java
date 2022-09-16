e551173726912979f550365a753ea9191a6e11bd
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-09-16 08:50:55 +0530

    it is udemy study Exercise.

--- src/Monitor.java
+++ src/Monitor.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Monitor {
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResoulation;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResoulation) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResoulation = nativeResoulation;
+    }
+    public void drawPixelAt(int x, int y, String color){
+        System.out.println("Drawing pixel at "+x+","+y+" in colour "+color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResoulation() {
+        return nativeResoulation;
+    }
 }
